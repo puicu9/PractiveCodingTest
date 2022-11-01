a00faf67751c6a7be8bfbe20457ddaffa3b6fd16
puicu9 <113329256+puicu9@users.noreply.github.com>
2022-11-01 21:26:16 +0900

    Initial commit

---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module myStudying {
+}
---
+++ src/programmers/Lv1/_221101/짝수와홀수.java
@@ -0,0 +1,25 @@
+package programmers.Lv1._221101;
+
+public class 짝수와홀수 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+	class Solution {
+	    public String solution(int num) {
+	        String answer = "";
+	        if(num%2 == 0){
+	            answer ="Even";
+	        } else {
+	            answer ="Odd";
+	        }
+	        
+	        
+	        return answer;
+	    }
+	}
+
+}
